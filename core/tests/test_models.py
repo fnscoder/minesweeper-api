93586a64317a589051e86efaa61afcc387b008f8
Felipe Souza <fnscoder@gmail.com>
2024-11-19 22:59:33 -0300

    Update tests

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -4,35 +4,45 @@ from django.utils import timezone
 
 
 class GameModelTest(TestCase):
+    """Test module for Game model"""
+
     def setUp(self):
+        """set up test creating a game"""
         self.game = Game.objects.create(rows=9, columns=9, mines=10, mode=GameMode.EASY)
 
     def test_game_is_active_by_default(self):
+        """Test if the game is active by default"""
         self.assertTrue(self.game.is_active())
 
     def test_game_str_method(self):
+        """Test the string representation of the game"""
         self.assertEqual(str(self.game), f"Game {self.game.id}")
 
     def test_game_duration_is_none_on_active_game(self):
+        """Test if the duration is None on an active game"""
         self.assertTrue(self.game.is_active())
         self.assertIsNone(self.game.finished_at)
         self.assertIsNone(self.game.duration)
 
     def test_game_duration_is_not_none_on_finished_game(self):
+        """Test if the duration is not None on a finished game"""
         self.game.status = GameStatus.WON
         self.game.finished_at = timezone.now()
         self.game.save()
+
+        self.assertFalse(self.game.is_active())
         self.assertIsNotNone(self.game.duration)
 
 
 class CellModelTest(TestCase):
-    def setUp(self):
-        self.game = Game.objects.create(
-            rows=9, columns=9, mines=10, mode=GameMode.EASY
-        )
-        self.cell = Cell.objects.create(
-            game=self.game, row=1, column=1, is_mine=True
-        )
+    """Test module for Game model"""
 
     def test_cell_str_method(self):
-        self.assertEqual(str(self.cell), f"Cell {self.cell.row}x{self.cell.column} - Game {self.cell.game.id}")
+        """Test the string representation of the cell"""
+        game = Game.objects.create(rows=9, columns=9, mines=10, mode=GameMode.EASY)
+        cell = Cell.objects.create(game=game, row=1, column=1, is_mine=True)
+
+        self.assertEqual(
+            str(cell),
+            f"Cell {cell.row}x{cell.column} - Game {cell.game.id}",
+        )
--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -15,113 +15,177 @@ from core.services import GameService
 
 
 class GameViewSetTest(TestCase):
+    """Test module for Game viewset"""
+
     def setUp(self):
+        """set up test creating a game and initialize cells"""
         self.client = APIClient()
         self.game = Game.objects.create(rows=9, columns=9, mines=10, mode=GameMode.EASY)
         GameService.initialize_cells(self.game)
+        self.url_list = reverse("game-list")
+        self.url_flag = reverse("game-flag", args=[self.game.id])
+        self.url_reveal = reverse("game-reveal", args=[self.game.id])
 
     def test_create_game_easy_mode(self):
-        url = reverse("game-list")
+        """Test creating a new game with easy mode"""
         data = {"mode": GameMode.EASY}
-        response = self.client.post(url, data, format="json")
+
+        response = self.client.post(self.url_list, data, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Game.objects.count(), 2)
 
     def test_create_game_custom_mode(self):
-        url = reverse("game-list")
+        """Test creating a new game with custom mode"""
         data = {"rows": 5, "columns": 5, "mines": 2, "mode": GameMode.CUSTOM}
-        response = self.client.post(url, data, format="json")
+
+        response = self.client.post(self.url_list, data, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Game.objects.count(), 2)
 
+    def test_retrieve_game(self):
+        """Test retrieving an existing game"""
+        url = reverse("game-detail", args=[self.game.id])
+        response = self.client.get(url, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["id"], self.game.id)
+
+    def test_retrieve_nonexistent_game(self):
+        """Test retrieving a nonexistent game"""
+        url = reverse("game-detail", args=[999])
+        response = self.client.get(url, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
     def test_create_game_custom_mode_invalid_options(self):
-        url = reverse("game-list")
+        """ " Test creating a new game with custom mode and invalid options"""
         data = {"rows": 5, "columns": 5, "mines": 30, "mode": GameMode.CUSTOM}
-        response = self.client.post(url, data, format="json")
+
+        response = self.client.post(self.url_list, data, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data["mines"][0], MINES_MUST_BE_SMALLER_THAN_CELLS)
 
     def test_flag_cell_active_game(self):
-        url = reverse("game-flag", args=[self.game.id])
+        """Test flagging a cell in an active game"""
         data = {"row": 1, "column": 1}
-        response = self.client.post(url, data, format="json")
+
+        response = self.client.post(self.url_flag, data, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["is_flagged"], True)
 
     def test_flag_cell_inactive_game(self):
+        """Test flagging a cell in an inactive game"""
         self.game.status = GameStatus.WON
         self.game.save()
-        url = reverse("game-flag", args=[self.game.id])
         data = {"row": 1, "column": 1}
-        response = self.client.post(url, data, format="json")
+
+        response = self.client.post(self.url_flag, data, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data, {"error": "Game is not active"})
+        self.assertEqual(response.data, GAME_NOT_ACTIVE)
 
     def test_flag_revealed_cell(self):
+        """Test flagging a revealed cell"""
         clean_cell = Cell.objects.filter(game=self.game, is_mine=False).first()
-        reveal_url = reverse("game-reveal", args=[self.game.id])
-        flag_url = reverse("game-flag", args=[self.game.id])
         data = {"row": clean_cell.row, "column": clean_cell.column}
-        self.client.post(reveal_url, data, format="json")
-        response = self.client.post(flag_url, data, format="json")
+        self.client.post(self.url_reveal, data, format="json")
+
+        response = self.client.post(self.url_flag, data, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data, {"error": "Cannot flag a revealed cell"})
+        self.assertEqual(response.data, CANNOT_FLAG_REVEALED_CELL)
 
     def test_flag_cell_nonexistent_cell(self):
-        url = reverse("game-flag", args=[self.game.id])
+        """Test flagging a nonexistent cell"""
         data = {"row": 99, "column": 99}
-        response = self.client.post(url, data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data, {"error": "Cell not found"})
+
+        response = self.client.post(self.url_flag, data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data, CELL_NOT_FOUND)
+
+    def test_unflag_flagged_cell_active_game(self):
+        """Test unflagging a flagged cell in an active game"""
+        data = {"row": 1, "column": 1}
+
+        response = self.client.post(self.url_flag, data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["is_flagged"], True)
+
+        response = self.client.post(self.url_flag, data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["is_flagged"], False)
 
     def test_reveal_clean_cell(self):
+        """Test revealing a clean cell"""
         clean_cell = Cell.objects.filter(game=self.game, is_mine=False).first()
-        url = reverse("game-reveal", args=[self.game.id])
         data = {"row": clean_cell.row, "column": clean_cell.column}
-        response = self.client.post(url, data, format="json")
+
+        response = self.client.post(self.url_reveal, data, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_reveal_nonexistent_cell(self):
-        url = reverse("game-reveal", args=[self.game.id])
+        """Test revealing a nonexistent cell"""
         data = {"row": 99, "column": 99}
-        response = self.client.post(url, data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data, {"error": "Cell not found"})
+
+        response = self.client.post(self.url_reveal, data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data, CELL_NOT_FOUND)
 
     def test_reveal_clean_cell_twice(self):
+        """Test revealing a clean cell"""
         clean_cell = Cell.objects.filter(game=self.game, is_mine=False).first()
-        url = reverse("game-reveal", args=[self.game.id])
         data = {"row": clean_cell.row, "column": clean_cell.column}
-        self.client.post(url, data, format="json")
-        response = self.client.post(url, data, format="json")
+
+        self.client.post(self.url_reveal, data, format="json")
+        response = self.client.post(self.url_reveal, data, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data, {"error": "Cell already revealed"})
+        self.assertEqual(response.data, CELL_ALREADY_REVEALED)
 
     def test_reveal_mine_cell(self):
+        """Test revealing a mine cell"""
         mine_cell = Cell.objects.filter(game=self.game, is_mine=True).first()
-        url = reverse("game-reveal", args=[self.game.id])
         data = {"row": mine_cell.row, "column": mine_cell.column}
-        response = self.client.post(url, data, format="json")
+
+        response = self.client.post(self.url_reveal, data, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["status"], GameStatus.LOST)
 
     def test_reveal_cell_inactive_game(self):
+        """Test revealing a cell in an inactive game"""
         self.game.status = GameStatus.WON
         self.game.save()
-        url = reverse("game-reveal", args=[self.game.id])
         data = {"row": 1, "column": 1}
-        response = self.client.post(url, data, format="json")
+
+        response = self.client.post(self.url_reveal, data, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data, {"error": "Game is not active"})
+        self.assertEqual(response.data, GAME_NOT_ACTIVE)
 
     def test_win_game(self):
+        """Test winning a game"""
         excluded_cell = Cell.objects.filter(game=self.game, is_mine=False).first()
-        clean_cells = Cell.objects.filter(game=self.game, is_mine=False).exclude(id=excluded_cell.id)
+        clean_cells = Cell.objects.filter(game=self.game, is_mine=False).exclude(
+            id=excluded_cell.id
+        )
         for cell in clean_cells:
             cell.is_revealed = True
             cell.save()
-        url = reverse("game-reveal", args=[self.game.id])
         data = {"row": excluded_cell.row, "column": excluded_cell.column}
-        response = self.client.post(url, data, format="json")
+
+        response = self.client.post(self.url_reveal, data, format="json")
+
         self.game.refresh_from_db()
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(self.game.status, GameStatus.WON)
